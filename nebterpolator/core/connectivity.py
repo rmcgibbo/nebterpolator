Internal hessian is now correct

nebterpolator/core/connectivity.py
@@ -18,8 +18,22 @@ from itertools import combinations, ifilter
 # these are covalent radii taken from the crystalographic data in nm
 # Dalton Trans., 2008, 2832-2838, DOI: 10.1039/B801115J
 # http://pubs.rsc.org/en/Content/ArticleLanding/2008/DT/b801115j
-COVALENT_RADII = {'C': 0.0762, 'N': 0.0706, 'O': 0.0661, 'H': 0.031,
-                  'S': 0.105}
+# COVALENT_RADII = {'C': 0.0762, 'N': 0.0706, 'O': 0.0661, 'H': 0.031,
+#                   'S': 0.105}
+
+COVALENT_RADII = {'H': 0.031, 'He': 0.028, 
+                  'Li': 0.128, 'Be': 0.096, 'B': 0.084, 'C': 0.076, 'N': 0.071, 'O': 0.066, 'F': 0.057, 'Ne': 0.058, 
+                  'Na': 0.166, 'Mg': 0.141, 'Al': 0.121, 'Si': 0.111, 'P': 0.107, 'S': 0.105, 'Cl': 0.102, 'Ar': 0.106, 
+                  'K': 0.203, 'Ca': 0.176, 'Sc': 0.170, 'Ti': 0.160, 'V': 0.153, 'Cr': 0.139, 'Mn': 0.161, 'Fe': 0.152, 'Co': 0.150, 
+                  'Ni': 0.124, 'Cu': 0.132, 'Zn': 0.122, 'Ga': 0.122, 'Ge': 0.120, 'As': 0.119, 'Se': 0.120, 'Br': 0.120, 'Kr': 0.116, 
+                  'Rb': 0.220, 'Sr': 0.195, 'Y': 0.190, 'Zr': 0.175, 'Nb': 0.164, 'Mo': 0.154, 'Tc': 0.147, 'Ru': 0.146, 'Rh': 0.142, 
+                  'Pd': 0.139, 'Ag': 0.145, 'Cd': 0.144, 'In': 0.142, 'Sn': 0.139, 'Sb': 0.139, 'Te': 0.138, 'I': 0.139, 'Xe': 0.140, 
+                  'Cs': 0.244, 'Ba': 0.215, 'La': 0.207, 'Ce': 0.204, 'Pr': 0.203, 'Nd': 0.201, 'Pm': 0.199, 'Sm': 0.198, 
+                  'Eu': 0.198, 'Gd': 0.196, 'Tb': 0.194, 'Dy': 0.192, 'Ho': 0.192, 'Er': 0.189, 'Tm': 0.190, 'Yb': 0.187, 
+                  'Lu': 0.187, 'Hf': 0.175, 'Ta': 0.170, 'W': 0.162, 'Re': 0.151, 'Os': 0.144, 'Ir': 0.141, 'Pt': 0.136, 
+                  'Au': 0.136, 'Hg': 0.132, 'Tl': 0.145, 'Pb': 0.146, 'Bi': 0.148, 'Po': 0.140, 'At': 0.150, 'Rn': 0.150, 
+                  'Fr': 0.260, 'Ra': 0.221, 'Ac': 0.215, 'Th': 0.206, 'Pa': 0.200, 'U': 0.196, 'Np': 0.190, 'Pu': 0.187, 
+                  'Am': 0.180, 'Cm': 0.169}
 
 __all__ = ['bond_connectivity', 'angle_connectivity', 'dihedral_connectivity']
 
@@ -157,6 +171,8 @@ def dihedral_connectivity(ibonds):
         for b in graph.neighbors(a):
             for c in ifilter(lambda c: c not in [a, b], graph.neighbors(b)):
                 for d in ifilter(lambda d: d not in [a, b, c], graph.neighbors(c)):
-                    idihedrals.append((a, b, c, d))
+                    quadruplet = (a, b, c, d) if a < d else (d, c, b, a)
+                    if quadruplet not in idihedrals:
+                        idihedrals.append(quadruplet)
 
     return np.array(idihedrals)

nebterpolator/core/internal_hessian.py
@@ -145,7 +145,7 @@ def dihedral_hessian(xyz, idihedrals):
     
     jacobian = np.zeros((n_dihedrals, n_atoms, 3))
     hessian = np.zeros((n_dihedrals, n_atoms, 3, n_atoms, 3))
-    
+
     for d, (m, o, p, n) in enumerate(idihedrals):
         u_prime = (xyz[m] - xyz[o])
         v_prime = (xyz[n] - xyz[p])
@@ -179,41 +179,43 @@ def dihedral_hessian(xyz, idihedrals):
         # END JACOBIAN
         
         term1 = sym_outer(cross_uw, w*cos_u - u) / (u_norm**2 * sin4_u)
-        term2 = sym_outer(cross_vw, w*cos_v - v) / (v_norm**2 * sin4_v)
+
+        # The plus sign here in w*cos_v + v is correct, the 2002 paper is wrong.
+        term2 = sym_outer(cross_vw, w*cos_v + v) / (v_norm**2 * sin4_v)
 
         term3 = sym_outer(cross_uw, w - 2*u*cos_u + w*cos_u**2) / (
                     2 * u_norm * w_norm * sin4_u)
+
         term4 = sym_outer(cross_vw, w + 2*v*cos_v + w*cos_v**2) / (
                     2 * v_norm * w_norm * sin4_v)
+
         term5 = sym_outer(cross_uw, u + u*cos_u**2 - 3*w*cos_u + w*cos_u**3) / (
                     2 * w_norm**2 * sin4_u)
         term6 = sym_outer(cross_vw, v + v*cos_v**2 + 3*w*cos_v - w*cos_v**3) / (
                     2 * w_norm**2 * sin4_v)    
-        term7 = (w*cos_u - u) / (u_norm * w_norm * np.sqrt(1-cos_u**2))
-        term8 = (w*cos_v - v) / (v_norm * w_norm * np.sqrt(1-cos_v**2))
+
+        term7 = (-w*cos_u + u) / (u_norm * w_norm * (1-cos_u**2))
+        term8 = (-w*cos_v - v) / (v_norm * w_norm * (1-cos_v**2))
         
-        for a in [m, n, o, p]:
-            for b in [m, n, o, p]:
+        for a in [m, o, p, n]:
+            for b in [m, o, p, n]:
                 hessian[d,a,:,b,:] += sign6(a,m,o, b,m,o) * term1
                 hessian[d,a,:,b,:] += sign6(a,n,p, b,n,p) * term2
                 hessian[d,a,:,b,:] += (sign6(a,m,o, b,o,p) + sign6(a,p,o, b,o,m)) * term3
                 hessian[d,a,:,b,:] += (sign6(a,n,p, b,p,o) + sign6(a,p,o, b,n,p)) * term4
                 hessian[d,a,:,b,:] += sign6(a,o,p, b,p,o) * term5
                 hessian[d,a,:,b,:] += sign6(a,o,p, b,o,p) * term6
-                
-                
-                if a != b:
-                    _term7 = (sign6(a,m,o, b,o,p) + sign6(a,p,o, b,o,m))*term7
-                    _term8 = (sign6(a,n,o, b,o,p) + sign6(a,p,o, b,o,m))*term8
-            
-                    for i in range(3):
-                        for j in range(3):
-                            for k in range(3):
-                                hessian[d, a, i, b, j] += (j-i)*(-1.0/2.0)*np.abs(j-i) * (_term7[k] + _term8[k])
-
-                        
-        return jacobian, hessian
-        
+                if (a, b) not in [(o,m), (p,m), (n,m), (p,o), (n,o), (n,p)]: continue
+                _term7 = sign6(a,p,o, b,o,m)*term7
+                _term8 = sign6(a,n,p, b,p,o)*term8
+                for i in range(3):
+                    for j in range(3):
+                        for k in range(3):
+                            if i != j and i != k and j != k:
+                                hessian[d, a, i, b, j] += (j-i)*(-1.0/2.0)**np.abs(j-i)*(_term7[k] + _term8[k])
+                                hessian[d, b, j, a, i] += (j-i)*(-1.0/2.0)**np.abs(j-i)*(_term7[k] + _term8[k])
+    return jacobian, hessian
+    
         
 if __name__ == '__main__':
     import internal_derivs